232662d5ec9fb6e3fbbc2a3e73fea924d055d708
Ousmane Ndiaye <ousmane.ndiaye6@orange-sonatel.com>
2020-02-23 01:57:48 +0000

    change theme

--- js/script.js
+++ js/script.js
@@ -1,290 +1,194 @@
-/*
---------------------------------------------------------
+$(document).on('ready', function(){
 
-[ INSTA , Javscript ]
+	/**** FILTER PORTFOLIO ****/
+	$('.section-portfolio .filter-portfolio li').on('click', function(){
+		//change active li filter
+		$('.section-portfolio .filter-portfolio li').removeClass('active');
+		$(this).addClass('active');
 
-  Template Name : INSTA - Responsive Onepage Resume Template
-  
-  Version       :  1.0 
-  
-  Author        :  Pixel_Factory
-  
-  Author URI    :  http://themeforest.net/user/Pixel_Factory
-  
-  Author Email  : h.nafees.anwar@gmail.com
+		//filter portfolio
+		var filter = $(this).attr('data-filter');
+		$('.section-portfolio .row > div').children().css({
+			'pointer-events':'none',
+			'opacity':'.4'
+		});
+		$('.section-portfolio .row > div').children(filter).css({
+			'pointer-events':'auto',
+			'opacity':'1'
+		});
+		if(filter == 'all') {
+	  		$('.section-portfolio .row > div').children().css({
+	  		'pointer-events':'auto',
+	  		'opacity':'1'
+	  		});
+  		}
+	});
 
---------------------------------------------------------
-*/
+	/**** Swiper Testimonials Slider ****/ /* more information : http://idangero.us/swiper/demos/#.V_0G2Tjy31V */
+	var swiper = new Swiper('.section-testimonials .swiper-container', {
+	    pagination: '.section-testimonials .swiper-pagination',
+	    paginationClickable: true,
+	    slidesPerView: 3,
+	    spaceBetween: 30,
+	    breakpoints: {
+	        1024: {
+	            slidesPerView: 3,
+	            spaceBetween: 40
+	        },
+	        768: {
+	            slidesPerView: 2,
+	            spaceBetween: 30
+	        },
+	        640: {
+	            slidesPerView: 1,
+	            spaceBetween: 20
+	        },
+	        320: {
+	            slidesPerView: 1,
+	            spaceBetween: 10
+	        }
+	    }
+	});
 
-/*  ------------------
-    Remove Preloader
-    ------------------  */
+	/**** Swiper Interests Slider ****/
+	var swiper = new Swiper('.section-interests .swiper-container', {
+	    pagination: '.section-interests .swiper-pagination',
+	    paginationClickable: true,
+	    slidesPerView: 6,
+	    spaceBetween: 30,
+	    breakpoints: {
+	        1024: {
+	            slidesPerView: 5,
+	            spaceBetween: 40
+	        },
+	        768: {
+	            slidesPerView: 4,
+	            spaceBetween: 30
+	        },
+	        640: {
+	            slidesPerView: 2,
+	            spaceBetween: 20
+	        },
+	        320: {
+	            slidesPerView: 1,
+	            spaceBetween: 10
+	        }
+	    }
+	});
 
-$(window).load(function () {
-		
-    $('#preloader').delay(350).fadeOut('slow', function () {
-        $('.profile-page, .resume-page, .contact-page, .portfolio-page').hide();
-    });
+	/**** MENU ****/
+	$('.menu a').on('click', function(e){
+		e.preventDefault();
+		//change active li menu
+		$('.menu a').removeClass('active');
+		$(this).addClass('active');
+
+		//scroll on click
+		$('html,body').animate({
+			scrollTop: $('#'+$(this).data('value')).offset().top - 20
+		},1500);
+	});
+
+	//open and close menu
+	$('.menu .open-menu').on('click', function(){
+		if($('.menu').css('left') == '-160px'){
+			$('.menu').css('left','0px');
+			$('.menu .open-menu i').attr('class','fa fa-times');
+		}
+		else if($('.menu').css('left') == '0px'){
+			$('.menu').css('left','-160px');
+			$('.menu .open-menu i').attr('class','fa fa-bars');
+		}
+	});
+
+	/**** SCROLL FUNCTION  ****/
+	$(window).on('scroll', function(){
+
+		//menu on scroll
+		var windowScroll = $(window).scrollTop();
+		$('.section').each(function(i){
+			if($(this).position().top <= windowScroll + 200){
+				$('.menu a.active').removeClass('active');
+				$('.menu a').eq(i).addClass('active');
+			}
+		});
+
+		// show and hiding button scroll top
+		if($(this).scrollTop()>= 1200)
+		{
+			$(".scroll-top").fadeIn(500);
+		}
+		else
+		{
+			$(".scroll-top").fadeOut(500);
+		}
+
+	});
+
+	//button scroll top on click
+	$(".scroll-top").on('click', function(){
+		$("html,body").animate({scrollTop: 0},1500);
+	});
+
+	/**** THEME-OPTION ****/
+
+	$('.theme-option .open-theme').on('click', function(){
+		if($('.theme-option').css('right') == '-227px'){
+			$('.theme-option').css('right','0');
+		}
+
+		else if($('.theme-option').css('right') == '0px'){
+			$('.theme-option').css('right','-227px');
+		}
+	});
+
+	$('.theme-option .light-dark-theme span').on('click', function(){
+		$('.theme-option .light-dark-theme i').css('opacity','0');
+		$(this).children('i').css('opacity','1');
+
+		var dataValue = $(this).data('value');
+		$('#light-dark').attr('href',dataValue);
+	});
+
+	$('.theme-option .colors-theme span').on('click', function(){
+		$('.theme-option .colors-theme i').css('opacity','0');
+		$(this).children('i').css('opacity','1');
+
+		var dataValue = $(this).data('value');
+		$('#colors').attr('href',dataValue);
+	});
+
+	/**** Magnific Popup ****/
+	var groups = {};
+	$('.section-portfolio .item-portfolio .work-popup').each(function() {
+	  var id = parseInt($(this).attr('data-group'), 10);
+	  
+	  if(!groups[id]) {
+	    groups[id] = [];
+	  }
+	  
+	  groups[id].push( this );
+	});
+	
+	$.each(groups, function() {
+	  
+	  $(this).magnificPopup({
+	      type: 'image',
+	      closeOnContentClick: true,
+	      closeBtnInside: false,
+	      gallery: { enabled:true }
+	  })
+	  
+	});
+
+	/**** WOW-ANIMATE ****/
+	new WOW().init();
+
 });
 
-$(document).ready(function () {
-
-    'use strict';
-
-    /*  ---------------------
-         Homepage Responsive
-        ---------------------  */
-
-
-    function homepageResponsive() {
-
-        // Homepage Main Portions Responsive
-
-        var windowsWidth = $(window).width(),
-            windowsHeight = $(window).height();
-
-        if (windowsWidth > windowsHeight) {
-
-            $('.introduction , .menu').css({
-                width: '50%',
-                height: '100%'
-            });
-
-        } else {
-
-            $('.introduction , .menu').css({
-                width: '100%',
-                height: '50%'
-            });
-
-        }
-
-        // Homepage Profile Image Responsive
-
-        var introWidth = $('.introduction').width(),
-            introHeight = $('.introduction').height(),
-            bgImage = $('.introduction').find('img');
-
-        if (introWidth > introHeight) {
-
-            bgImage.css({
-                width: '100%',
-                height: 'auto'
-            });
-
-        } else {
-
-            bgImage.css({
-                width: 'auto',
-                height: '100%'
-            });
-
-        }
-
-    }
-
-    $(window).on('load resize', homepageResponsive);
-
-    /*  --------------
-         Menu Settings
-        --------------  */
-
-    // Hide Menu
-
-    $('.menu > div').on('click', function () {
-
-        var introWidth = $('.introduction').width(),
-            menuWidth = $('.menu').width();
-
-        $('.introduction').animate({
-            left: '-' + introWidth
-        }, 1000, 'easeOutQuart');
-        $('.menu').animate({
-            left: menuWidth
-        }, 1000, 'easeOutQuart', function () {
-            $('.home-page').css({
-                visibility: 'hidden'
-            });
-        });
-
-    });
-
-    // Show Reletive Page Onclick
-
-    $('.menu div.profile-btn').on('click', function () {
-        $('.profile-page').fadeIn(1200);
-    });
-
-    $('.menu div.resume-btn').on('click', function () {
-        $('.resume-page').fadeIn(1200);
-    });
-
-    $('.menu div.portfolio-btn').on('click', function () {
-        $('.portfolio-page').fadeIn(1200);
-    });
-
-    $('.menu div.contact-btn').on('click', function () {
-        $('.contact-page').fadeIn(1200);
-    });
-
-    // Close Button, Hide Menu
-
-    $('.close-btn').on('click', function () {
-        $('.home-page').css({
-            visibility: 'visible'
-        });
-        $('.introduction, .menu').animate({
-            left: 0
-        }, 1000, 'easeOutQuart');
-        $('.profile-page, .resume-page, .portfolio-page, .contact-page').fadeOut(800);
-    });
-
-    /*  --------------------------------
-         Maximize Services Items Height
-        --------------------------------  */
-
-    function maximizeHeight() {
-
-        var minHeight = 0;
-
-        $('.services').each(function () {
-
-            var maxHeight = $(this).height();
-
-            if (maxHeight > minHeight) {
-                minHeight = maxHeight;
-            }
-
-        });
-
-        $('.services').height(minHeight);
-    }
-
-    maximizeHeight();
-
-    $(window).on('resize', maximizeHeight);
-
-    /*  ----------------------------------------
-         Tooltip Starter for Social Media Icons
-        ----------------------------------------  */
-
-    $('.intro-content .social-media [data-toggle="tooltip"]').tooltip({
-        placement: 'bottom'
-    });
-
-    $('.contact-details .social-media [data-toggle="tooltip"]').tooltip();
-
-    /*  ------------
-         Pie Charts
-        ------------  */
-
-    $(function () {
-
-        var fillColor = $('.footer').css('background-color'); // Get background color of footer to set relevent fill color in pie charts
-
-        // Initiate EasyPieChart
-
-        $('.skill').easyPieChart({
-            barColor: fillColor,
-            trackColor: '#c6c6c6',
-            scaleColor: '#c6c6c6',
-            scaleLength: 8,
-            lineWidth: 8,
-            size: 150,
-            lineCap: 'butt'
-        });
-
-    });
-
-    /*  -------------------------------
-         Filterizer ( for portfolio page )
-        -------------------------------  */
-
-    $(function () {
-        
-        var filterizd = $('.projet-items').filterizr({});
-
-
-    });
-
-    /*  -------------------------------
-         PopUp ( for portfolio page )
-        -------------------------------  */
-
-    $(function () {
-        $('.show-popup').popup({
-            keepInlineChanges: true,
-            speed: 500
-        });
-    });
-
-    /*  -----------------------------------------------------
-          OwlCarousel ( for portfolio page : testimonials slider )
-        -----------------------------------------------------  */
-
-    $(function () {
-        $(".owl-carousel").owlCarousel({
-            navigation: false, // Show next and prev buttons
-            slideSpeed: 200,
-            paginationSpeed: 300,
-            singleItem: true
-        });
-    });
-
-    /*  -------------
-         Contact Form
-        ------------- */
-
-    $('#contactForm').submit(function () {
-
-        $.ajax({
-            type: "POST",
-            url: "php/contact.php",
-            data: $('#contactForm').serialize(),
-            success: function (msg) {
-                if (msg == 'SEND') {
-                    $('.success').fadeIn();
-                    $('.error').fadeOut();
-                    $('#contactForm')[0].reset();
-                } else {
-                    $('.success').fadeOut();
-                    $('.error').fadeIn().find('h3').text(msg);
-                }
-            }
-        });
-        return false;
-    });
-
-    /*  -------------------------------
-         Google Map ( for contact page )
-        -------------------------------  */
-
-    $('#google-map').gMap({
-        latitude: 31.562130,
-        longitude: 74.319460,
-        maptype: 'TERRAIN',
-        scrollwheel: false,
-        zoom: 14,
-        markers: [
-            {
-                latitude: 31.562130,
-                longitude: 74.319460,
-                html: "I am Here!",
-                icon: {
-                    image: "images/icon/map_marker.png",
-                    iconsize: [46, 46],
-                    iconanchor: [12, 46]
-                }
-            }
-        ],
-        controls: {
-            panControl: false,
-            zoomControl: true,
-            mapTypeControl: false,
-            scaleControl: false,
-            streetViewControl: false,
-            overviewMapControl: false
-        }
-    });
-
+/**** LOADER ****/
+$(window).on('load', function()
+{
+  $("body").css("overflow","auto");
+  $(".loading-overlay, .loading-overlay").fadeOut();
 });
